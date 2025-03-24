127a81a33d12b6ff0c60c205345cd7c548a6f904
Avishka Chamod <avishkachamod14@gmail.com>
2025-03-24 16:55:29 +0530

    fifth-commit

--- home.php
+++ home.php
@@ -27,27 +27,316 @@
 
 
             <!-- main banner -->
-            <div class="col-lg-8 col-md-12 col-sm-12 main-banner-container">
+            <div class="col-lg-9 col-md-12 col-sm-12 main-banner-container">
                 <div class="row">
 
-                        <img  class="main-banner-img-container" src="img/banners/Plant-Shop-Singapore-Your-One-Stop-Destination-for-Indoor-and-Outdoor-Plants.jpg" alt="">
+                    <div id="carouselExampleAutoplaying" class="carousel slide main-banner-img-container" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="img/banners/Plant-Shop-Singapore-Your-One-Stop-Destination-for-Indoor-and-Outdoor-Plants.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/banners/Plant-Shop-Singapore-Your-One-Stop-Destination-for-Indoor-and-Outdoor-Plants.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/banners/Plant-Shop-Singapore-Your-One-Stop-Destination-for-Indoor-and-Outdoor-Plants.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
 
-        
+                    <hr>
+
+                    <div class="link-img-container">
+                        <div class="text-center">
+                            <a href="#">
+                                <img src="img/link-logos/4tr.jpg">
+                            </a>
+
+                            <a href="#">
+                                <img src="img/link-logos/df.jpg">
+                            </a>
+
+                            <a href="">
+                                <img src="img/link-logos/fgf.jpg">
+                            </a>
+
+                            <a href="#">
+                                <img src="img/link-logos/gfg.jpg">
+                            </a>
+
+                            <a href="#">
+                                <img src="img/link-logos/R.jpg">
+                            </a>
+                        </div>
+                    </div>
+
+                    <hr>
+
+
                 </div>
             </div>
             <!-- main banner -->
 
-             <!-- second banner -->
-              <div class="col-lg-4 col-md-12 col-sm-12 second-banner-container">
+            <!-- second banner -->
+            <div class="col-lg-3 col-md-12 col-sm-12 second-banner-container">
                 <div class="row">
-                   <img src="img/banners/2.jpg" style="height: 400px;" class="pb-3">
-                   <img src="img/banners/2.jpg" style="height: 400px;" class="pb-3">
+                    <img src="img/banners/2.jpg" class="pb-3">
+                    <img src="img/banners/2.jpg" class="pb-3">
                 </div>
 
-              </div>
-             <!-- second banner -->
+            </div>
+            <!-- second banner -->
 
-             
+            <!-- main body -->
+            <div class="col-12 ">
+                <div class="row">
+
+                    <!-- featured products -->
+                    <div class="col-12">
+                        <div class="row">
+                            <h1>Featured Products</h1>
+
+                            <div class="fp-product-container p-4">
+
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="product-card">
+                                    <img src="img/webimg/icon.ico" alt="Hot Deal" class="badge">
+                                    <img src="img/link-logos/4tr.jpg" alt="Product" class="product-image">
+                                    <div class="product-details">
+                                        <div class="product-title">Awesome Product</div>
+                                        <div class="stars-product">★★★★★</div>
+                                        <div class="price-product">$29.99</div>
+                                        <div class="buttons-product">
+                                            <button class="btn-product-buy">Buy Now</button>
+                                            <button class="btn-product-cart">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <!-- featured products -->
+
+                    <!-- second Carousel -->
+                    <div class="col-12 second-add-area p-5">
+                        <div class="row">
+
+                            <div class="col-12 second-carousel">
+                                <div class="row ">
+                                    <div id="carouselExampleFade" class="carousel slide carousel-fade">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img src="img/banners/Plant-Shop-Singapore-Your-One-Stop-Destination-for-Indoor-and-Outdoor-Plants.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="img/banners/Plant-Shop-Singapore-Your-One-Stop-Destination-for-Indoor-and-Outdoor-Plants.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="img/banners/2.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!-- second Carousel -->
+
+                    <!-- shop by categories -->
+                    <div class="col-12 ">
+                        <div class="row">
+
+                            <h1> Shop By Categories</h1>
+
+                            <div class="cat-tab-container">
+
+                                <div class="cat-tab col-2 text-center p-3 m-3">
+                                    <a href="#"> Test</a>
+                                </div>
+
+                                <div class="cat-tab col-2 text-center p-3 m-3">
+                                    <a href="#"> Test</a>
+                                </div>
+
+                                <div class="cat-tab col-2 text-center p-3 m-3">
+                                    <a href="#"> Test</a>
+                                </div>
+
+                                <div class="cat-tab col-2 text-center p-3 m-3">
+                                    <a href="#"> Test</a>
+                                </div>
+
+                                <div class="cat-tab col-2 text-center p-3 m-3">
+                                    <a href="#"> Test</a>
+                                </div>
+
+                            </div>
+
+
+
+
+                        </div>
+
+                    </div>
+
+                    <!-- shop by categories -->
+
+
+                </div>
+
+            </div>
+            <!-- main body -->
+
+
 
 
 
@@ -56,7 +345,9 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
     <script src="script.js"></script>
 
 </body>
